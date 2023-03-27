f5e9ebea2a074253365ce554dab612f980e8885e
JackHoward <jhoward000@live.com>
2023-03-27 13:10:34 -0500

    1.01 Init commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using NLog;
 
 // See https://aka.ms/new-console-template for more information
-string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
 
 // create instance of Logger
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
@@ -10,5 +10,14 @@ logger.Info("Program started");
 string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
 logger.Info(scrubbedFile);
 MovieFile movieFile = new MovieFile(scrubbedFile);
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+ // LINQ - Where filter operator & Contains quantifier operator
+ var Movies = movieFile.Movies.Where(m => m.title.Contains("(1990)"));
+ // LINQ - Count aggregation method
+ Console.WriteLine($"There are {Movies.Count()} movies from 1990");
+
+ Console.ForegroundColor = ConsoleColor.White;
 
 logger.Info("Program ended");
